afdb1fafb15408b8e5068048cd15d5e6b972c2e2
Taufan FP Ruslanali <tfpruslanali@gmail.com>
2025-02-04 18:49:39 +0800

    Modify script for execution delay (try add &)

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -21,9 +21,9 @@ set -x
 npm start &
 sleep 1
 echo $! > .pidfile
-set +x
-
+set +x &
 sleep 60    # Jeda aplikasi selama 1 menit setelah berhasil di deploy
+
 echo 'Now...'
 echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
 echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
