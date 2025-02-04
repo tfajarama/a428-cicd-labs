9613dd63c844694fcee82daa64eb7b0387df8b38
Taufan FP Ruslanali <tfpruslanali@gmail.com>
2025-02-04 18:41:27 +0800

    Modify script for kill delay

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+# Jeda aplikasi selama 1 menit setelah berhasil di deploy
+sleep 60
+
 echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
